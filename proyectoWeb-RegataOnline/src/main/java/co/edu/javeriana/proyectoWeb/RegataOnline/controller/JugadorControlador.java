39e74caa96de44008b4c0b0172f51c7544ebc3a9
SamuOsorio <osorio.samuel@gmail.com>
2025-08-26 17:49:34 -0500

    feat: Edit option, falta crear y borrar

--- proyectoWeb-RegataOnline/src/main/java/co/edu/javeriana/proyectoWeb/RegataOnline/controller/JugadorControlador.java
+++ proyectoWeb-RegataOnline/src/main/java/co/edu/javeriana/proyectoWeb/RegataOnline/controller/JugadorControlador.java
@@ -8,12 +8,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import co.edu.javeriana.proyectoWeb.RegataOnline.dto.JugadorDTO;
 import co.edu.javeriana.proyectoWeb.RegataOnline.model.Jugador;
 import co.edu.javeriana.proyectoWeb.RegataOnline.services.JugadorServicio;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 
 @Controller
@@ -27,7 +31,7 @@ public class JugadorControlador {
     @GetMapping("/list")
     public ModelAndView listarJugadores() {
         log.info("Lista de Jugadores");
-        List<Jugador> jugadores = jugadorServicio.listarJugadores();
+        List<JugadorDTO> jugadores = jugadorServicio.listarJugadores();
         ModelAndView modelAndView = new ModelAndView("jugador-lista");
         modelAndView.addObject("listaJugadores", jugadores);
         return modelAndView;
@@ -35,11 +39,40 @@ public class JugadorControlador {
 
     @GetMapping("/view/{idJugador}")   
     public ModelAndView buscarJugador(@PathVariable("idJugador") Long id){
-        Jugador jugador = jugadorServicio.buscarJugador(id);
+        JugadorDTO jugador = jugadorServicio.buscarJugador(id).orElseThrow();
         ModelAndView modelAndView = new ModelAndView("jugador-view");
         modelAndView.addObject("jugador", jugador);
         return modelAndView;
     }
+
+    @GetMapping("/create")   
+    public ModelAndView formularioCrearJugador(){
+        ModelAndView modelAndView = new ModelAndView("jugador-edit");
+        modelAndView.addObject("jugador", new JugadorDTO());
+        return modelAndView;
+    }
+
+    @GetMapping("/edit/{id}")   
+    public ModelAndView formularioEditarJugador(@PathVariable Long id){
+        JugadorDTO jugadorDTO = jugadorServicio.buscarJugador(id).orElseThrow();
+        ModelAndView modelAndView = new ModelAndView("jugador-edit");
+        modelAndView.addObject("jugador", jugadorDTO);
+        return modelAndView;
+    }
+
+    @PostMapping("/save")
+    public RedirectView guardarJugador(@ModelAttribute JugadorDTO jugadorDTO){
+        jugadorServicio.guardarJugador(jugadorDTO);
+        return new RedirectView("/jugador/list");
+    }
+
+    @GetMapping("/delete/{id}")
+    public RedirectView borrarJugador(@PathVariable long id){
+        jugadorServicio.borrarJugador(id);
+        return new RedirectView("/jugador/list");
+    }
+
+
     
     
 }
--- proyectoWeb-RegataOnline/src/main/java/co/edu/javeriana/proyectoWeb/RegataOnline/services/JugadorServicio.java
+++ proyectoWeb-RegataOnline/src/main/java/co/edu/javeriana/proyectoWeb/RegataOnline/services/JugadorServicio.java
@@ -1,10 +1,13 @@
 package co.edu.javeriana.proyectoWeb.RegataOnline.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import co.edu.javeriana.proyectoWeb.RegataOnline.dto.JugadorDTO;
+import co.edu.javeriana.proyectoWeb.RegataOnline.mapper.JugadorMapper;
 import co.edu.javeriana.proyectoWeb.RegataOnline.model.Jugador;
 import co.edu.javeriana.proyectoWeb.RegataOnline.repository.JugadorRepositorio;
 
@@ -13,12 +16,23 @@ public class JugadorServicio {
     @Autowired
     private JugadorRepositorio jugadorRepositorio;
 
-    public List<Jugador> listarJugadores(){
-        return jugadorRepositorio.findAll();
+    public List<JugadorDTO> listarJugadores(){
+        return jugadorRepositorio.findAll().stream().map(JugadorMapper::toDTO).toList();
+
     }
 
-    public Jugador buscarJugador(Long id){
-        return jugadorRepositorio.findById(id).orElseThrow();
+    public Optional<JugadorDTO> buscarJugador(Long id){
+        return jugadorRepositorio.findById(id).map(JugadorMapper::toDTO);
     }
     
+    public void guardarJugador(JugadorDTO jugadorDTO){
+        Jugador jugador = JugadorMapper.toEntity(jugadorDTO);
+        jugadorRepositorio.save(jugador);
+    }
+
+    public void borrarJugador(long id) {
+        jugadorRepositorio.deleteById(id);
+    }
+
+
 }
